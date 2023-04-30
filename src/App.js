4a05839fc7b4a3d791a75e739f566f0a5d18c2be
Tingwutobong Okonson <tidarkson@gmail.com>
2023-04-30 01:56:39 +0100

    loading screen

--- src/App.js
+++ src/App.js
@@ -5,21 +5,45 @@ import Product from "./Pages/Product";
 import ProductList from "./Pages/ProductList";
 import Register from "./Pages/Register";
 import ScrollToTop from "./components/ScrollToTop";
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import BounceLoader from "react-spinners/BounceLoader";
+import { useState, useEffect } from "react";
 
 
 
 function App() {
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 5000)
+  }, [])
+
+
   return (
     <>
-      <BrowserRouter>
-      <ScrollToTop/>
-        <Routes>
-          <Route path='/' element= {<Homepage/>} />
-          <Route path='/product-list' element= {<ProductList/>} />
-          <Route path='/:name' element= {<Product/>} />
-        </Routes>
-      </BrowserRouter>
+      {
+        loading ?
+          <div className="flex flex-col items-center justify-center text-center w-full h-full"><BounceLoader
+            color={'#BCD8C1'}
+            loading={loading}
+            size={150}
+            aria-label="Loading Spinner"
+            data-testid="loader"/>
+            <h1 className='uppercase text-2xl text-slate-900 text-center xl:text-start font-extrabold tracking-widest'><span className="logo">➰</span>style loop</h1>
+          </div>
+          :
+          <BrowserRouter>
+            <ScrollToTop />
+            <Routes>
+              <Route path='/' element={<Homepage />} />
+              <Route path='/product-list' element={<ProductList />} />
+              <Route path='/:name' element={<Product />} />
+            </Routes>
+          </BrowserRouter>
+      }
       {/* <Register/> */}
       {/* <Login/> */}
       {/* <Cart/> */}
---
+++ src/Pages/Loader.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Loader() {
+  return (
+    <>
+        <div>
+        <h1 className='uppercase text-6xl text-center font-extrabold tracking-widest'><span className="logo">➰</span>style loop</h1>
+        </div>
+    </>
+  )
+}
+
+export default Loader
